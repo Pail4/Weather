5dbdb71af5719e18b856390215f47ed5e48dbc48
Pail4 <73517684+Pail4@users.noreply.github.com>
2021-12-19 12:39:12 +0400

    github https bug fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { UI } from "./view.js";
 
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
 
 let locationList = ["Perm"]
